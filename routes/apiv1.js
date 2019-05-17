2b6ec4e572b1977e1dee703115b09457bf726cc4
DD PP <declanpenwarden@yahoo.co.nz>
2019-05-17 02:15:17 +0000

    wad

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -26,10 +26,10 @@ exports.getWeather = function(req, res) {
     }, function(err, resp, body) {
     	if(err) {
     		res.status(400).send('Failed to get the data');
-    		//console.error("Failed to send request to openweathermap.org", err);
+ 
     	} else {
     		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + 'C';
     			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
     		} else {
@@ -60,7 +60,7 @@ exports.getWeather2 = function(req, res) {
     		//console.error("Failed to send request to openweathermap.org", err);
     	} else {
     		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
     			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
     		} else {
