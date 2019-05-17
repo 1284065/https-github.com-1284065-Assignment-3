a425b91b69b0b778715cf58a2d4b6352ad1f58d4
DD PP <declanpenwarden@yahoo.co.nz>
2019-05-17 01:25:30 +0000

    t6

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -11,7 +11,7 @@ var request = REQUEST.defaults( {
 
 //var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?id=524901&APPID={6b7b471967dd0851d0010cdecf28f829}";
 //var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6f9927c97f4a2621ce91e7477afaac42&q={city name}";
-var OPENWEATHERURL ="http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
+//var OPENWEATHERURL ="http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
 //&units=metric";
 
 exports.getWeather = function(req, res) {
